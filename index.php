528815a6b2523cef9800e6c6d02abeadad6166dc
Chell2358 <forestser00@gmail.com>
2023-08-02 20:43:40 +0900

    tests

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php
-
-
-
-phpinfo();
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP 프로그램</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world";
+        ?>
+    </body>
+</html>
